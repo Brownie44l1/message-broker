212c2dcffa217f0341c13ee5a3f6e683f9322332
Brownie44l1 <apataomotayo0@gmail.com>
2025-11-19 08:42:10 +0100

    WIP:step 1

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 type MessageBroker struct {
-	[]messages{}
+	messages []string
+}
+
+func (m *MessageBroker) publish(message string) {
+	m.messages = append(m.messages, message)
+}
+
+func (m *MessageBroker) consume() (string, error) {
+	if len(m.messages) == 0 {
+		return "", errors.New("Empty list")
+	} else {
+		msg := m.messages[0]
+		m.messages = m.messages[1:]
+		return msg, nil
+	}
 }
 
 func main() {
+	broker := &MessageBroker{}
+
+	broker.publish("Hello")
+	broker.publish("World")
+
+	msg, err := broker.consume()
 	
+	if err != nil {
+		fmt.Println("Error: ", err)
+	} else {
+		fmt.Println("Got Message: ", msg)
+	}
 }
