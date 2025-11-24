e645aae3a2f129bd85b02f479b0df31e366ae6d9
Brownie44l1 <apataomotayo0@gmail.com>
2025-11-24 13:02:39 +0100

    refactor...

--- numbers-processor/main.go
+++ numbers-processor/main.go
@@ -6,36 +6,50 @@ import (
 )
 
 type Number struct {
+	Index    int
 	Original int
 	Squared  int
 }
 
 func generator(out chan<- Number) {
 	for i := 1; i <= 10; i++ {
-		out <- Number{Original: i, Squared: 0}
+		out <- Number{Index: i - 1, Original: i}
 	}
 	close(out)
 }
 
 func processor(in <-chan Number, out chan<- Number) {
+	workerOut := make(chan Number)
 	var wg sync.WaitGroup
 	numWorkers := 3
 
 	wg.Add(numWorkers)
 	for i := 0; i < numWorkers; i++ {
 		go func() {
+			defer wg.Done()
 			for n := range in {
 				n.Squared = n.Original * n.Original
-				out <- n
+				workerOut <- n
 			}
-			wg.Done()
 		}()
 	}
 
 	go func() {
 		wg.Wait()
-		close(out)
+		close(workerOut)
 	}()
+
+	results := make(map[int]Number)
+
+	for n := range workerOut {
+		results[n.Index] = n
+	}
+
+	for i := 0; i < len(results); i++ {
+        out <- results[i]
+    }
+
+	close(out)
 }
 
 func printer(in <-chan Number, out chan<- Number) {
