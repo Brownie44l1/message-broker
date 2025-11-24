e4d5f2c59b8163f5b51d496a87815fa648f6550e
Brownie44l1 <apataomotayo0@gmail.com>
2025-11-24 12:12:05 +0100

    refactor; practicing channels

--- numbers-processor/main.go
+++ numbers-processor/main.go
@@ -1,55 +1,52 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func generator(out chan<- int) {
+type Number struct {
+	Original int
+	Squared  int
+}
+
+func generator(out chan<- Number) {
 	for i := 1; i <= 10; i++ {
-		out <- i
+		out <- Number{Original: i, Squared: 0}
 	}
 	close(out)
 }
 
-func receiver(in <- chan int, out chan <- int) {
-	for v := range in {
-		out <- v * v
+func processor(in <-chan Number, out chan<- Number) {
+	for n := range in {
+		n.Squared = n.Original * n.Original
+		out <- n
 	}
 	close(out)
 }
 
-func printer(in <- chan int, out chan <- int) {
-	for squared := range in {
-		original := 0
-		for i := 1; i*i <= squared; i++ {
-			if i*i == squared {
-				original = i
-				break
-			}
-		}
-		fmt.Printf("Received: %d -> Squared: %d\n", original, squared)
-		out <- squared
+func printer(in <-chan Number, out chan<- Number) {
+	for n := range in {
+		fmt.Printf("Received: %d, Squared: %d\n", n.Original, n.Squared)
+		out <- n
 	}
 	close(out)
 }
 
-func sum(in <- chan int) {
-	var sum int
-	for v := range in {
-		sum += v
+func sum(in <-chan Number) {
+	total := 0
+	for n := range in {
+		total += n.Squared
 	}
-	fmt.Printf("The total sum of squared results is: %d\n", sum)
+	fmt.Printf("Total sum of the squared numbers is: %d\n", total)
 }
 
 func main() {
-	a := make(chan int)
-	b := make(chan int)
-	c := make(chan int)
+	ch1 := make(chan Number)
+	ch2 := make(chan Number)
+	ch3 := make(chan Number)
 
-	go generator(a)
-	go receiver(a, b)
-	go printer(b, c)
-	sum(c)
+	go generator(ch1)
+	go processor(ch1, ch2)
+	go printer(ch2, ch3)
+	sum(ch3)
 
-	fmt.Println("All done")
+	fmt.Println("Refactored: all done.")
 }
